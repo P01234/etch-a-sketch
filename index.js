404e965fe3a50beb15a1bb83f5b2e44a7dc4276a
Pedro Kauã <pk.bleite@gmail.com>
2022-08-26 00:14:04 -0300

    incompleteProject

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
+//If you're seing this and find something that is weird or could be done better with another way.
+//You are propably right as this is one of my first projects.
+//PROJETO INCOMPLETO.
 const selectColorButton = document.querySelector("#pickColorButton").value;
-const rainbow = document.querySelector("rainbowButton");
-const eraser = document.querySelector("eraserButton");
-const clear = document.querySelector("clearButton");
+const rainbow = document.querySelector("#rainbowButton").addEventListener("click", rainbowOption);
+const eraser = document.querySelector("#eraserButton");
+const clear = document.querySelector("#clearButton").addEventListener('click', clean);
 const rows = document.querySelector("#setGrid").value;
-const columns = rows;
-const mainContainer = document.querySelector("#mainContainer");
+const mainContent = document.getElementById("mainContainer");
+const mainContent2 =  document.getElementById("mainContainer");
+let createDiv
+
 //make the grid.
 
 
-let makeGrid = function(row, column) {
-    mainContainer.style.setProperty("--grid-rows",row);
-    mainContainer.style.setProperty("--grid-cols", column);
-    for(let i = 0; i <= (row * column); i++)    {
-        let createDiv = document.createElement("div");
-        mainContainer.appendChild(createDiv);
-        console.log(i);
-
+let makeGrid = function(size) {
+    for(let i = 0; i < (size * size); i++){
+      let createDiv2 = document.createElement("div");
+        mainContent.appendChild(createDiv2)
+        mainContent.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        mainContent.style.gridTemplateRows = `repeat(${size}, 1fr)`;      
+        createDiv2.classList.add('box');
+        createDiv = createDiv2;
+        createDiv2.addEventListener("mouseover", () => {
+            createDiv2.style.backgroundColor = selectColorButton;
+           
+        })
     }
+
+
 };
-    makeGrid(rows, columns); 
+    console.log(createDiv)
+    makeGrid(rows); 
+    function rainbowOption()    {
+        alert("jorge!!!");
+    }
+    
+    
+
+
+
+   
+    function clean() {
+        let a;
+        a = mainContent.style.backgroundColor = "white";
+    }
+    
+    
+    
+    
+    
+    
 
 
