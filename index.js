bec2d7041054fc2907b7e3e9a705349525d952e2
Pedro Kauã <pk.bleite@gmail.com>
2022-08-29 19:39:17 -0300

    etch-a-sketch

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
- 
-//If you're seing this and find something that is weird or could be done better with another way.
-//You are propably right as this is one of my first projects.
-//PROJETO INCOMPLETO.
-const selectColorButton = document.querySelector("#pickColorButton").value;
-const clear = document.querySelector("#clearButton").addEventListener('click', clean);
-let rows = document.querySelector("#setGrid").value;
-let whatSize = document.querySelector("#whatSize").innerHTML = rows;
+//Defining important variables
+let modeInUse = "color";
+const color = document.querySelector(".firstOptionButton").value;
+const clearColor = "#fff"
+const colorButton = document.querySelector("#colorButton").addEventListener("click", () => changeMode("color"));
+const rainbowButton = document.querySelector("#rainbowButton").addEventListener("click", () => changeMode("rainbow"));
+const eraserButton = document.querySelector("#eraserButton").addEventListener("click", () => changeMode("eraser"));
+const clear = document.querySelector("#clearButton").addEventListener("click", clearPage);
 const mainContent = document.getElementById("mainContainer");
-const mainContent2 =  document.getElementById("mainContainer");
-let createDivContainer = undefined;
-let i = undefined;
-let randomColor = undefined;
-//make the grid.
+const gridSize = prompt('Choose the size of your grid');
+let rainbowColor = ["#9400D3" , "#4B0082" , "#0000FF" , "#00FF00", 
+"#FFFF00", "#FF7F00", "#FF0000" ];
 
 
-let makeGrid = function(size) {
-    for(i = 0; i < (size * size); i++){
-      let createDiv = document.createElement("div");
-        mainContent.appendChild(createDiv)
-        mainContent.style.width = "960px";
-        mainContent.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        mainContent.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-        createDiv.classList.add('box');
-        createDivContainer = createDiv;
-        createDiv.addEventListener("mousemove", () => {  
-          createDiv.style.backgroundColor = selectColorButton;
-            
-            
-        })
 
-};
-}
-    makeGrid(rows);
+
+function changeMode(newMode)  {
+  if(newMode == 'color'){
+     modeInUse = newMode;
     
+  }else if(newMode == "rainbow"){
+     modeInUse = newMode;
+    
+  }else{
+     modeInUse = newMode;
+    
+  }
+  return colorGrid(modeInUse);
+}
 
+function makeGrid(size) {
+  for(let i = 0; i < (size * size); i++)  {
+  
+    let gridCreator = document.createElement('div');
+    mainContent.appendChild(gridCreator);
+    mainContent.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    mainContent.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    mainContent.classList.add('box');
+    gridCreator.style.border = "black solid 1px";
+    gridCreator.addEventListener("mouseover", () => colorGrid(modeInUse))
+   
+    
+    
+  }
+}
+makeGrid(gridSize);
 
+function colorGrid(whatMode){
+  let a = mainContent.querySelectorAll("div");
+  if(whatMode == "color") {
+    a.forEach(item => {
+        item.addEventListener("mouseover", () =>{
+        item.style.backgroundColor = color
+      })
+    })
+  }else if(whatMode == "rainbow"){
+    a.forEach(item => {
+      item.addEventListener("mouseover", () => {
+        const fR = Math.floor(Math.random() * 256)
+        const sG = Math.floor(Math.random() * 256)
+        const tB = Math.floor(Math.random() * 256)
+        item.style.backgroundColor = `rgb(${fR}, ${sG}, ${tB})`;
+      })
+    })
+  }
+  else{
+    a.forEach(item => {
+      item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "#fff";
+      })
+    })
+  }
+}
 
-
-
-    function clean() {
-        mainContent.innerHTML = "";
-        makeGrid(rows);
-    }
-
-
-
-
-
-
+function clearPage()  {
+  let clear;
+  clear = mainContent.querySelectorAll("div");
+  clear.forEach(all => {
+    all.style.backgroundColor = clearColor;
+  })
+}
 
 
